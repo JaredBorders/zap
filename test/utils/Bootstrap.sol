3ef35ac910fcd8dc0d5a7080bc19f8e04f7e9bc3
JaredBorders <jaredborders@pm.me>
2023-12-20 18:40:13 -0500

    ✅ Update bootstrap

--- test/utils/Bootstrap.sol
+++ test/utils/Bootstrap.sol
@@ -1,66 +1,63 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.18;
+pragma solidity 0.8.20;
 
 import {console2} from "lib/forge-std/src/console2.sol";
 import {
-    Counter,
-    OptimismGoerliParameters,
-    OptimismParameters,
+    Zap,
+    BaseGoerliParameters,
+    BaseParameters,
     Setup
 } from "script/Deploy.s.sol";
+import {MockSpotMarketProxy} from "test/utils/mocks/MockSpotMarketProxy.sol";
+import {MockUSDC} from "test/utils/mocks/MockUSDC.sol";
+import {MockSUSD} from "test/utils/mocks/MockSUSD.sol";
 import {Test} from "lib/forge-std/src/Test.sol";
 
 contract Bootstrap is Test {
     using console2 for *;
 
-    Counter internal counter;
+    Zap internal zap;
 
     function initializeLocal() internal {
         BootstrapLocal bootstrap = new BootstrapLocal();
-        (address counterAddress) = bootstrap.init();
+        (address zapAddress) = bootstrap.init();
 
-        counter = Counter(counterAddress);
+        zap = Zap(zapAddress);
     }
 
-    function initializeOptimismGoerli() internal {
-        BootstrapOptimismGoerli bootstrap = new BootstrapOptimismGoerli();
-        (address counterAddress) = bootstrap.init();
+    function initializeBase() internal {
+        BootstrapBase bootstrap = new BootstrapBase();
+        (address zapAddress) = bootstrap.init();
 
-        counter = Counter(counterAddress);
+        zap = Zap(zapAddress);
     }
 
-    function initializeOptimism() internal {
-        BootstrapOptimismGoerli bootstrap = new BootstrapOptimismGoerli();
-        (address counterAddress) = bootstrap.init();
+    function initializeBaseGoerli() internal {
+        BootstrapBaseGoerli bootstrap = new BootstrapBaseGoerli();
+        (address zapAddress) = bootstrap.init();
 
-        counter = Counter(counterAddress);
+        zap = Zap(zapAddress);
     }
-
-    /// @dev add other networks here as needed (ex: Base, BaseGoerli)
 }
 
 contract BootstrapLocal is Setup {
-    function init() public returns (address) {
-        address counterAddress = Setup.deploySystem();
-
-        return counterAddress;
+    function init() public returns (address zapAddress) {
+        zapAddress = Setup.deploySystem(
+            address(new MockUSDC()),
+            address(new MockSUSD()),
+            address(new MockSpotMarketProxy())
+        );
     }
 }
 
-contract BootstrapOptimism is Setup, OptimismParameters {
-    function init() public returns (address) {
-        address counterAddress = Setup.deploySystem();
-
-        return counterAddress;
+contract BootstrapBase is Setup, BaseParameters {
+    function init() public returns (address zapAddress) {
+        zapAddress = Setup.deploySystem(USDC, USD_PROXY, SPOT_MARKET_PROXY);
     }
 }
 
-contract BootstrapOptimismGoerli is Setup, OptimismGoerliParameters {
-    function init() public returns (address) {
-        address counterAddress = Setup.deploySystem();
-
-        return counterAddress;
+contract BootstrapBaseGoerli is Setup, BaseGoerliParameters {
+    function init() public returns (address zapAddress) {
+        zapAddress = Setup.deploySystem(USDC, USD_PROXY, SPOT_MARKET_PROXY);
     }
 }
-
-// add other networks here as needed (ex: Base, BaseGoerli)
