96394b74b2dd41d2d98745a2d83278b23b38463f
JaredBorders <jaredborders@pm.me>
2023-12-20 18:40:01 -0500

    ✅ Create mocks for local tests

---
+++ test/utils/mocks/MockUSDC.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.20;
+
+import {IERC20} from "./../../../src/interfaces/IERC20.sol";
+
+contract MockUSDC is IERC20 {
+    function balanceOf(address) external pure override returns (uint256) {
+        return 0;
+    }
+
+    function transfer(address, uint256) external pure override returns (bool) {
+        return true;
+    }
+
+    function approve(address, uint256) external pure override returns (bool) {
+        return true;
+    }
+
+    function transferFrom(address, address, uint256)
+        external
+        pure
+        override
+        returns (bool)
+    {
+        return true;
+    }
+}
