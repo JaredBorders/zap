af42c38a73fa0f6bf4f1c55dce157a977104408b
JaredBorders <jaredborders@pm.me>
2023-12-25 21:22:25 -0600

    👷🏻‍♂️ Update natspec

--- src/ZapEvents.sol
+++ src/ZapEvents.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.20;
 
+/// @title Zap contract events
+/// @author JaredBorders (jaredborders@pm.me)
 contract ZapEvents {
     /// @notice emitted after successful $USDC -> $sUSD zap
     /// @param amount of $USDC wrapped and $sUSD minted
