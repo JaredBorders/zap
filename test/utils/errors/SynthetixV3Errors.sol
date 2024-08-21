85f84f57ed68a2ffc32f1e168ce3ee725e714d64
jaredborders <44350516+JaredBorders@users.noreply.github.com>
2024-08-21 16:26:12 -0400

    ⚙️ bump solc version

--- test/utils/errors/SynthetixV3Errors.sol
+++ test/utils/errors/SynthetixV3Errors.sol
@@ -1,10 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.20;
+pragma solidity 0.8.26;
 
 /// @title Cosolidated Errors from Synthetix v3 contracts
-/// @notice This contract consolidates all necessary errors from Synthetix v3
-/// contracts
-/// and is used for testing purposes
 /// @author JaredBorders (jaredborders@pm.me)
 contract SynthetixV3Errors {
 
