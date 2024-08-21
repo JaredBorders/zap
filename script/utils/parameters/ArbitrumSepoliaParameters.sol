905b7cb56ab4fd35a982af8e38baf2bf1a515f28
jaredborders <44350516+JaredBorders@users.noreply.github.com>
2024-08-21 16:25:30 -0400

    🚀 update deployment params

--- script/utils/parameters/ArbitrumSepoliaParameters.sol
+++ script/utils/parameters/ArbitrumSepoliaParameters.sol
@@ -1,20 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.20;
+pragma solidity 0.8.26;
 
-contract ArbitrumSepoliaParameters {
-
-    // https://arbiscan
-    address public constant USDC = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
-
-    // https://arbiscan
-    address public constant SPOT_MARKET_PROXY =
-        0x18141523403e2595D31b22604AcB8Fc06a4CaA61;
-
-    // https://arbiscan
-    address public constant USD_PROXY =
-        0x09d51516F38980035153a554c26Df3C6f51a23C3;
-
-    // https://arbiscan
-    uint128 public constant SUSDC_SPOT_MARKET_ID = 1;
-
-}
+contract ArbitrumSepoliaParameters {}
