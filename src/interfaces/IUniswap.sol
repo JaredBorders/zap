2f5dc7ce0022c90d1e448de3a9db296660f7b867
Moss <moss@cc.snxdao.io>
2024-10-15 13:54:21 -0400

    👷 fixed IUniswap Quoter interface

--- src/interfaces/IUniswap.sol
+++ src/interfaces/IUniswap.sol
@@ -46,12 +46,22 @@ interface IQuoter {
     /// @param path The path of the swap, i.e. each token pair and the pool fee
     /// @param amountIn The amount of the first token to swap
     /// @return amountOut The amount of the last token that would be received
+    /// @return sqrtPriceX96AfterList List of the sqrt price after the swap for
+    /// each pool in the path
+    /// @return initializedTicksCrossedList List of the initialized ticks that
+    /// the swap crossed for each pool in the path
+    /// @return gasEstimate The estimate of the gas that the swap consumes
     function quoteExactInput(
         bytes memory path,
         uint256 amountIn
     )
         external
-        returns (uint256 amountOut);
+        returns (
+            uint256 amountOut,
+            uint160[] memory sqrtPriceX96AfterList,
+            uint32[] memory initializedTicksCrossedList,
+            uint256 gasEstimate
+        );
 
     /// @notice Returns the amount in required for a given exact output swap
     /// without executing the swap
@@ -59,11 +69,21 @@ interface IQuoter {
     /// Path must be provided in reverse order
     /// @param amountOut The amount of the last token to receive
     /// @return amountIn The amount of first token required to be paid
+    /// @return sqrtPriceX96AfterList List of the sqrt price after the swap for
+    /// each pool in the path
+    /// @return initializedTicksCrossedList List of the initialized ticks that
+    /// the swap crossed for each pool in the path
+    /// @return gasEstimate The estimate of the gas that the swap consumes
     function quoteExactOutput(
         bytes memory path,
         uint256 amountOut
     )
         external
-        returns (uint256 amountIn);
+        returns (
+            uint256 amountIn,
+            uint160[] memory sqrtPriceX96AfterList,
+            uint32[] memory initializedTicksCrossedList,
+            uint256 gasEstimate
+        );
 
 }
