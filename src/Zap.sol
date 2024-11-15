8866723d0a36b3ebe6677a0c13df719421fc3c14
Moss <moss@cc.snxdao.io>
2024-11-15 18:26:36 +0700

    fmt

--- src/Zap.sol
+++ src/Zap.sol
@@ -511,7 +511,9 @@ contract Zap is Reentrancy, Errors, Flush(msg.sender) {
             odosSwap(_collateral, _swapAmountIn, _path);
             unwound -= _swapAmountIn;
             uint256 leftovers = IERC20(USDC).balanceOf(address(this));
-            if (leftovers > _flashloan) _push(USDC, _receiver, leftovers - _flashloan);
+            if (leftovers > _flashloan) {
+                _push(USDC, _receiver, leftovers - _flashloan);
+            }
         }
 
         /// @notice the path and max amount in must take into consideration:
