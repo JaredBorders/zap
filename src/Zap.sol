a6a24fcb2ebb96c2f6c49f0f2d4ec2650579afdc
Flocqst <flocastet@icloud.com>
2025-03-13 16:27:08 +0100

    👷 _zapOutUSDx

--- src/Zap.sol
+++ src/Zap.sol
@@ -228,8 +228,8 @@ contract Zap is Reentrancy, Errors, Flush(msg.sender) {
         internal
         returns (uint256 zapped)
     {
-        zapped = _buy(SSTATA_SPOT_ID, _amount, _minAmountOut);
-        zapped = _unwrap(SSTATA_SPOT_ID, zapped, _minAmountOut);
+        zapped = _buy(SUSDC_SPOT_ID, _amount, _minAmountOut);
+        zapped = _unwrap(SUSDC_SPOT_ID, zapped, _minAmountOut);
     }
 
     /*//////////////////////////////////////////////////////////////
